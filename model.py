39c48659b3a758db86e252d9295dc15955bd0957
Karan Anchan <kar.anchan02@gmail.com>
2024-05-17 09:22:00 +0530

    Positional Encodings Initialized

--- model.py
+++ model.py
@@ -12,3 +12,29 @@ class InputEmbeddings(nn.Module):
 
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.d_model)
+    
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model: int, seq_len: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.dropout = nn.Dropout(dropout)
+
+        # Matrix Of Shape (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
+        # Vector Of Shape (seq_len, 1)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        
+        # Sine Function applied on Even Positions
+        pe[:, 0::2] = torch.sin(position * div_term)
+        # Cosine Function applied on Odd Positions
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        # Expand Dimension to include Batches (1, seq_len, d_model)
+        pe = pe.unsqueeze(0)
+
+        # Register the Tensor in Buffer to Save Along the Model
+        self.register_buffer('pe', pe)
+
