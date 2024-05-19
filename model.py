e88bdc3cc49031ec4f0e3e66d2d8b8eefa7a8eb2
Karan Anchan <kar.anchan02@gmail.com>
2024-05-19 13:57:52 +0530

    MultiHeadAttention Initialized

--- model.py
+++ model.py
@@ -66,3 +66,19 @@ class FeedForwardBlock(nn.Module):
 
     def forward(self, x):
         return self.linear_2(self.dropout(torch.relu(self.linear_1(x))))
+    
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_model: int, h: int, dropout: float):
+        super().__init__()
+        self.d_model = d_model
+        self.h = h
+
+        assert d_model % h == 0, "d_model not divisible by h"
+        self.d_k = d_model // h
+
+        self.w_q = nn.Linear(d_model, d_model) # Wq
+        self.w_k = nn.Linear(d_model, d_model) # Wk
+        self.w_v = nn.Linear(d_model, d_model) # Wv
+
+        self.w_o = nn.Linear(d_model, d_model) # Wo
+        self.dropout = nn.Dropout(dropout)
