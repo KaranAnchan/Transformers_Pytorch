2155a0ad4dc56f56aaf171eb56b948cd527556d7
Karan Anchan <kar.anchan02@gmail.com>
2024-05-21 17:21:08 +0530

    Added docstrings to the train functions

--- train.py
+++ train.py
@@ -11,14 +11,33 @@ from tokenizers.pre_tokenizers import BertPreTokenizer
 
 def get_all_sentences(ds, lang):
     
-    
+    """
+    Generator function that yields sentences in a specified language from the dataset.
+
+    Args:
+        ds (Dataset): The dataset containing multilingual translations.
+        lang (str): The language code of the sentences to yield.
+
+    Yields:
+        str: Sentences in the specified language.
+    """
     
     for item in ds:
         yield item['translation'][lang]
 
 def get_or_build_tokenizer(config, ds, lang):
     
-    
+    """
+    Retrieves an existing tokenizer or trains a new tokenizer on the dataset if it doesn't exist.
+
+    Args:
+        config (dict): Configuration dictionary containing the tokenizer file path and language codes.
+        ds (Dataset): The dataset containing multilingual translations.
+        lang (str): The language code for which to build the tokenizer.
+
+    Returns:
+        Tokenizer: The trained tokenizer for the specified language.
+    """
     
     tokenizer_path = Path(config['tokenizer_file'].format(lang))
     if not Path.exists(tokenizer_path):
@@ -36,7 +55,16 @@ def get_or_build_tokenizer(config, ds, lang):
 
 def get_ds(config):
     
-    
+    """
+    Loads the dataset, trains or loads tokenizers for the source and target languages, 
+    and splits the dataset into training and validation sets.
+
+    Args:
+        config (dict): Configuration dictionary containing language codes and tokenizer file paths.
+
+    Returns:
+        tuple: A tuple containing the training and validation datasets, and the tokenizers for source and target languages.
+    """
     
     ds_splitted = load_dataset('cfilt/iitb-english-hindi')
     ds_raw = concatenate_datasets([ds_splitted['train'], ds_splitted['test'], ds_splitted['validation']])
