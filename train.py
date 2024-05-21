0f4401f34fa2727e0b90a9cf1ccb558b423ce3a2
Karan Anchan <kar.anchan02@gmail.com>
2024-05-21 14:36:10 +0530

    Built Tokenizer Function

---
+++ train.py
@@ -0,0 +1,27 @@
+import torch
+import torch.nn as nn
+from pathlib import Path
+
+from datasets import load_dataset
+from tokenizers import Tokenizer
+from tokenizers.models import BPE
+from tokenizers.trainers import BpeTrainer
+from tokenizers.pre_tokenizers import BertPreTokenizer
+
+def get_or_build_tokenizer(config, ds, lang):
+    
+    
+    
+    tokenizer_path = Path(config['tokenizer_file'].format(lang))
+    if not Path.exists(tokenizer_path):
+        tokenizer = Tokenizer(BPE(unk_token='[UNK]'))
+        tokenizer.pre_tokenizer = BertPreTokenizer()
+        trainer = BpeTrainer(special_tokens=["[UNK]", "[PAD]", "[SOS]", "[EOS]"], 
+                             min_frequency=2)
+        tokenizer.train_from_iterator(get_all_sentences(ds, lang), trainer=trainer)
+        tokenizer.save(str(tokenizer_path))
+    
+    else:
+        tokenizer = Tokenizer.from_file(str(tokenizer_path))
+        
+    return tokenizer
