9be35995c6bf295e1b825a81f3939d00977b758f
Karan Anchan <kar.anchan02@gmail.com>
2024-05-23 16:14:43 +0530

    Added docstrings for the functions

--- config.py
+++ config.py
@@ -2,7 +2,16 @@ from pathlib import Path
 
 def get_config():
     
-    
+    """
+    Returns the configuration settings for the training process.
+
+    This function provides a dictionary of hyperparameters and settings used for training
+    a machine learning model, including parameters for batch size, learning rate, sequence length,
+    model dimensions, source and target languages, and file paths for saving models and tokenizers.
+
+    Returns:
+        dict: A dictionary containing the configuration settings.
+    """
     
     return {
         "batch_size" : 8,
@@ -21,7 +30,20 @@ def get_config():
     
 def get_weights_file_path(config, epoch: str):
     
-    
+    """
+    Constructs the file path for saving or loading model weights.
+
+    Given the configuration settings and the epoch number, this function generates the appropriate
+    file path for saving or loading model weights. The path is constructed based on the model folder,
+    basename, and epoch provided.
+
+    Args:
+        config (dict): The configuration dictionary containing settings including the model folder and basename.
+        epoch (str): The epoch number to include in the filename.
+
+    Returns:
+        str: The constructed file path for the model weights.
+    """
     
     model_folder = config['model_folder']
     model_basename = config['model_basename']
