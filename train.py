e3b782e2f26ed6688aebbb74bbdffa6719d25947
Karan Anchan <88701179+KaranAnchan@users.noreply.github.com>
2024-05-25 11:04:51 +0530

    Update train.py

--- train.py
+++ train.py
@@ -244,7 +244,7 @@ def filter_long_sentences(dataset_split):
         
         en_len = len(item['translation']['en'].split())
         hi_len = len(item['translation']['hi'].split())
-        if en_len <= 30 and hi_len <= 30:
+        if en_len <= 20 and hi_len <= 20:
             filtered_data['translation'].append(item['translation'])
             
     return filtered_data
