6bb5552a05ac309aeb61590428a275a51b44a239
Karan Anchan <kar.anchan02@gmail.com>
2024-06-05 13:51:54 +0530

    Added Visualization and Inference Notebook

--- config.py
+++ config.py
@@ -17,13 +17,13 @@ def get_config():
         "batch_size" : 8,
         "num_epochs" : 30,
         "lr" : 10**-4,
-        "seq_len": 150,
+        "seq_len": 128,
         "d_model": 512,
         "lang_src" : "en",
         "lang_tgt" : "hi",
         "model_folder" : "weights",
         "model_basename" : "tmodel_",
-        "preload" : None,
+        "preload" : "07",
         "tokenizer_file" : "tokenizer_{0}.json",
         "experiment_name" : "runs/tmodel"
     }
